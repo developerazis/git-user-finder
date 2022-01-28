aab15507ea2c0cfaf885b2d7d492d6679bd6ddf1
developerazis <developerazis@gmail.com>
2022-01-28 10:43:13 +0700

    update users model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -86,32 +86,3 @@ class User {
     return data;
   }
 }
-
-class UserSearch {
-  late int totalCount;
-  bool? incompleteResults;
-  List<User>? users;
-
-  UserSearch({this.incompleteResults, this.users});
-
-  UserSearch.fromJson(Map<String, dynamic> json) {
-    totalCount = json['total_count'];
-    incompleteResults = json['incomplete_results'];
-    if (json['users'] != null) {
-      users = <User>[];
-      json['items'].forEach((v) {
-        users!.add(new User.fromJson(v));
-      });
-    }
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['total_count'] = this.totalCount;
-    data['incomplete_results'] = this.incompleteResults;
-    if (this.users != null) {
-      data['items'] = this.users!.map((v) => v.toJson()).toList();
-    }
-    return data;
-  }
-}
