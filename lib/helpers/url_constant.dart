42ab24f178a63d11b82b0914331a2ff4178ee6e4
developerazis <developerazis@gmail.com>
2022-01-29 09:58:43 +0700

    create service for search user

--- lib/helpers/url_constant.dart
+++ lib/helpers/url_constant.dart
@@ -2,4 +2,6 @@ class UrlConstant {
   static const String BASE_URL = "https://api.github.com/";
   String getAllUser(since, perPage) =>
       "${BASE_URL}users?since=$since&per_page=$perPage";
+  String getUser(name, page, perPage) =>
+      "${BASE_URL}search/users?q=$name&page=$page&per_page=$perPage";
 }
--- lib/services/user_services.dart
+++ lib/services/user_services.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:github_user_finder_app/helpers/url_constant.dart';
 import 'package:github_user_finder_app/models/all_user.dart';
+import 'package:github_user_finder_app/models/user_find.dart';
 
 class UserService {
   Dio _dio = Dio();
@@ -13,6 +14,20 @@ class UserService {
       _allUser = AllUser.fromJson(_response.data);
 
       return _allUser;
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<UserFind?> getUser(
+      {required name, int page = 1, int perPage = 30}) async {
+    Response? _response;
+    UserFind? userFind;
+    try {
+      _response = await _dio.get(UrlConstant().getUser(name, page, perPage));
+      userFind = UserFind.fromJson(_response.data);
+
+      return userFind;
     } catch (e) {
       print(e);
     }
